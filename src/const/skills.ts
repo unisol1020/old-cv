3c0d1b844f30aa6416b341c019bbea1b6e9335a3
unisol1020 <unisol1020@icloud.com>
2024-08-16 17:54:14 +0300

    [CHANGED] expo icon url

--- src/const/skills.ts
+++ src/const/skills.ts
@@ -4,117 +4,137 @@ const skills = [
     description:
       "JavaScript is a programming language that conforms to the ECMAScript specification.",
     icon: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png?20120221235433",
+    from: 2020,
   },
   {
     label: "TypeScript",
     description:
       "TypeScript is a programming language developed and maintained by Microsoft.",
     icon: "https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg",
+    from: 2021,
   },
   {
     label: "PHP",
     description:
       "PHP is a general-purpose scripting language geared towards web development.",
     icon: "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg",
+    from: 2020,
   },
   {
     label: "Node.js",
     description:
       "Node.js is an open-source, cross-platform, back-end JavaScript runtime environment.",
     icon: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg",
+    from: 2022,
   },
   {
     label: "React",
     description:
       "React is an open-source, front end, JavaScript library for building user interfaces or UI components.",
     icon: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+    from: 2022,
   },
   {
     label: "React Native",
     description:
       "React Native is an open-source mobile application framework created by Facebook.",
     icon: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+    from: 2023,
   },
   {
     label: "Expo",
     description:
       "Expo is an open-source platform for making universal native apps for Android, iOS, and the web with JavaScript and React.",
-    icon: "https://scontent-waw2-1.xx.fbcdn.net/v/t39.30808-6/292449664_534790351778780_8076701901009628740_n.png?_nc_cat=106&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=JuL8njOrTfcQ7kNvgHT4gF1&_nc_ht=scontent-waw2-1.xx&oh=00_AYBJJcwZEU70kq9_6yQu_8kSKyBP1gPyuJeH8bJgJufA_A&oe=66B685B8",
+    icon: "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*M4W1zKMuf0xmAcZwZgUcTQ.png",
+    from: 2023,
   },
   {
     label: "Next.js",
     description: "Next.js, the most popular React framework.",
     icon: "https://pbs.twimg.com/profile_images/1565710214019444737/if82cpbS_400x400.jpg",
+    from: 2022,
   },
   {
     label: "Angular",
     description:
       "Angular is a TypeScript-based open-source web application framework led by the Angular Team at Google.",
     icon: "https://upload.wikimedia.org/wikipedia/commons/c/cf/Angular_full_color_logo.svg",
+    from: 2021,
   },
   {
     label: "Nest.js",
     description:
       "Nest.js is a back-end framework for building efficient, reliable and scalable server-side applications.",
     icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/NestJS.svg/1242px-NestJS.svg.png?20221211225055",
+    from: 2022,
   },
   {
     label: "Express.js",
     description:
       "Express.js is a back-end framework for building web applications.",
     icon: "https://images.credly.com/images/1c2c86e1-16ce-4e4d-a425-d1ac96bb026d/express.png",
+    from: 2022,
   },
   {
     label: "Laravel",
     description:
       "Laravel is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications.",
     icon: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Laravel.svg",
+    from: 2021,
   },
   {
     label: "Type ORM",
     description: "TypeORM is an ORM that can run in NodeJS.",
     icon: "https://avatars.githubusercontent.com/u/20165699?s=200&v=4",
+    from: 2022,
   },
   {
     label: "Drizzle ORM",
     description: "Drizzle is an ORM that can run in NodeJS.",
     icon: "https://pbs.twimg.com/profile_images/1767809210060877824/mAtEmNk0_400x400.jpg",
+    from: 2024,
   },
   {
     label: "Cypress",
     description:
       "Cypress is a front end automated testing application created for the modern web.",
     icon: "https://avatars.githubusercontent.com/u/8908513?s=200&v=4",
+    from: 2023,
   },
   {
     label: "PostgreSQL",
     description:
       "PostgreSQL is a powerful, open-source object-relational database system.",
     icon: "https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg",
+    from: 2020,
   },
   {
     label: "MySQL",
     description:
       "MySQL is an open-source relational database management system.",
     icon: "https://www.mysql.com/common/logos/logo-mysql-170x115.png",
+    from: 2020,
   },
   {
     label: "MongoDB",
     description:
       "MongoDB is a source-available cross-platform document-oriented database program.",
     icon: "https://upload.wikimedia.org/wikipedia/commons/9/93/MongoDB_Logo.svg",
+    from: 2022,
   },
   {
     label: "Docker",
     description:
       "Docker is a set of platform as a service products that use OS-level virtualization to deliver software in packages called containers.",
     icon: "https://upload.wikimedia.org/wikipedia/commons/7/79/Docker_%28container_engine%29_logo.png",
+    from: 2022,
   },
   {
     label: "Python",
     description:
       "Python is an interpreted high-level general-purpose programming language.",
     icon: "https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg",
+    from: 2023,
   },
 ];
 
