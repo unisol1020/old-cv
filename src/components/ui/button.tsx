fe99cbc14b8e5139163578a82b3aea0adc310ffd
unisol1020 <unisol1020@icloud.com>
2024-09-16 17:16:23 +0300

    Update button size and padding in button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -22,7 +22,7 @@ const buttonVariants = cva(
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
-        lg: "h-11 p-6",
+        lg: "h-11 p-7",
         icon: "h-10 w-10",
       },
     },
