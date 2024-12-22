8649b4d2834ddf3517e490c2555b984ee99e1ac7
unisol1020 <unisol1020@icloud.com>
2024-12-22 12:25:55 +0200

    Update logo URL for Yukon Software in experience.ts

--- src/const/experience.ts
+++ src/const/experience.ts
@@ -5,7 +5,7 @@ const experience = [
     location: "Chernivtsi, Ukraine",
     startDate: "02.2020",
     endDate: "Present",
-    logo: "https://media.licdn.com/dms/image/C4D0BAQF1q05i5S77JQ/company-logo_200_200/0/1639068666718/yukon_software_ukraine_logo?e=1730937600&v=beta&t=isRfiFi6kS3_M6yIV1BgQE2PNKB0CKRcUbSvhKaxx0E",
+    logo: "https://yukon.software/wp-content/themes/wp-yukon-theme/assets/images/Yukon-logo.png",
   },
 ];
 
