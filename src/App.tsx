98ed3baaaa9106b4b75889f255f4d2a4732264f1
unisol1020 <unisol1020@icloud.com>
2025-04-14 18:53:07 +0300

    Refactor routing in App component: replace RouterProvider with BrowserRouter and Routes; remove routes.tsx file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes";
-
-const router = createBrowserRouter(routes);
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ExperiencePage from "./routes/experience";
+import HomePage from "./routes/home";
+import Root from "./routes/root";
+import SkillsPage from "./routes/skills";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Root />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/skills" element={<SkillsPage />} />
+          <Route path="/experience" element={<ExperiencePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
