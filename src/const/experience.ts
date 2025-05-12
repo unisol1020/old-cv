7e856a6b2bdfc64a1cf5d8804018d8ad8eeeaf5a
unisol1020 <unisol1020@icloud.com>
2025-05-12 10:08:03 +0300

    Update end date for Yukon Software experience in experience.ts to reflect new timeline: changed from "Present" to "05.2025".

--- src/const/experience.ts
+++ src/const/experience.ts
@@ -1,10 +1,10 @@
 const experience = [
   {
-    title: "Software Developer (React, Next.js,Angular, Node.js)",
+    title: "Software Developer (React, Next.js, Angular, Node.js)",
     company: "Yukon Software",
     location: "Chernivtsi, Ukraine",
     startDate: "02.2020",
-    endDate: "Present",
+    endDate: "05.2025",
     logo: "https://yukon.software/wp-content/themes/wp-yukon-theme/assets/images/Yukon-logo.png",
   },
 ];
